5239f2bea3702fc915b412298c2cdb4f6b43c68f
alilein1990 <alina.leinweber@gmx.de>
2025-04-16 22:51:10 +0200

    case study: rest api / crud / mongo / auth

--- src/components/work/apiCaseStudy.jsx
+++ src/components/work/apiCaseStudy.jsx
@@ -5,6 +5,7 @@ import ExtensionIcon from '@mui/icons-material/Extension'; // Puzzle-like
 import LockIcon from '@mui/icons-material/Lock';
 import BuildIcon from '@mui/icons-material/Build';
 import FlagIcon from '@mui/icons-material/Flag';
+import StorageIcon from '@mui/icons-material/Storage';
 
 import './apiCaseStudy.scss'
 
@@ -35,7 +36,7 @@ const ApiCaseStudy = ({ language, isWidthGreaterThan1050 }) => {
             <div  >
                 <h2><span className='span_title'>{Texts[language].case_api.title.charAt(0)}</span>{Texts[language].case_api.title.slice(1)}</h2>
             </div>
-            <img src={`${process.env.PUBLIC_URL}/img/case-study_movie-api.jpg`} alt='CaseStudyPic' className='case-study-pic' />
+            {/* <img src={`${process.env.PUBLIC_URL}/img/case-study_movie-api.jpg`} alt='CaseStudyPic' className='case-study-pic' /> */}
             <div className="table-wrapper">
                 <table>
                     <thead>
@@ -54,33 +55,162 @@ const ApiCaseStudy = ({ language, isWidthGreaterThan1050 }) => {
                     </tbody>
                 </table>
             </div>
-            <div className='case-study-svgs-div'>
-                <CodeIcon className="case-study-svgs" />
-                <p>
-                    Rest API
-                </p>
-                <ExtensionIcon className="case-study-svgs" />
-                <p>
-                    CRUD & Mongo
-                </p>
-                <LockIcon className="case-study-svgs" />
-                <p>
-                    Authentication
-                </p>
-                <BuildIcon className="case-study-svgs" />
-                <p>
-                    Debugging
-                </p>
-                <FlagIcon className="case-study-svgs" />
-                <p>
-                    Final Conclusion
-                </p>
-            </div>
+            <div className='case-study-svgs-wrapper'>
+                <div className='case-study-section'>
+                    <CodeIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+                        <p className='case-study-header-p'>Rest API</p>
+                        <div class="rest-api-diagram">
+                            <p>A REST API is like a messenger between two different systems that lets them communicate.
+                            </p>
+                            <div class="top-row">
+                                <div class="box">CLIENT</div>
+                                <div className="request-row">
+                                    <p class="request-row-p">REQUEST</p>
+                                    {/* width & height removed for better controll */}
+                                    <svg viewBox="0 0 150 80" xmlns="http://www.w3.org/2000/svg">
+                                        <line x1="10" y1="10" x2="130" y2="10" stroke="black" stroke-width="2" />
+                                        <polygon points="130,5 130,15 140,10" fill="black" />
+                                        {/* arrow to left  */}
+                                        {/* <polygon points="10,5 10,15 0,10" fill="black" /> */}
+                                    </svg>
 
-            <button onClick={goBack} className="details_link">
+                                </div>
+                                <div class="box">SERVER</div>
+                            </div>
+
+                            <div class="bottom-row">
+                                <div class="response-row">
+                                    <div class="arrow-label">RESPONSE</div>
+                                    <svg viewBox="0 0 300 80" width="100%" height="auto" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M10 0 V60 H290 V0" stroke="black" stroke-width="2" fill="none" />
+                                    </svg>
+
+                                </div>
+                                <div class="database">DATABASE</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='case-study-section'>
+                    <ExtensionIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+
+                        <p className='case-study-header-p'>
+                            CRUD
+                        </p>
+                        <div class="crud-section">
+                            <p>CRUD stands for Create, Read, Update, and Delete. These four operations allow data to be added, viewed, changed, or removed from the database in software applications. This is used when building API endpoints.</p>
+                            <span class="crud-text">CRUD IN ACTION</span>
+                            <p>The documentation is always there to show how the endpoint that calls a CRUD operation is structured:</p>
+
+                            <div class="glow-frame">
+                                <div class="glow-inner">
+                                    <img src={`${process.env.PUBLIC_URL}/img/CRUD-endpoint.jpg`} className='case-study-pic-crud' alt="CRUD Endpoint" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div className='case-study-section'>
+                    <StorageIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+                        <p className='case-study-header-p'>
+                            Mongo Database
+                        </p>
+                        <div className='mongo-section'>
+                            <p>
+                                In a software application, a database is used to store all kinds of information, like  product catalogs.</p>
+                            <p>A MongoDB database stores this data in an easy-to-read format, similar to a list:</p>
+                            <pre>
+                                <code>
+                                    {`{
+  "_id": "ObjectId('65a9b1abeb496f127613d9b8')",
+  "Title": "The Green Mile",
+  "Genre": {
+    "Name": "Drama",
+    "Description": "Drama is a category of narrative fiction intended to be more serious than humorous in tone."
+  },
+  "Director": {
+    "Name": "Frank Darabont",
+    "Bio": "Hungarian-American film director, screenwriter, and producer.",
+    "Birth": "1959",
+    "Death": null
+  },
+  "ImagePath": "https://image.tmdb.org/t/p/original/veWPhVMQeQKcxggNEU8YmIo52R.jpg",
+  "Featured": true,
+  "Description": "The lives of guards on Death Row are affected by one of their charges: a man accused of child murder and rape, yet who has a mysterious gift.",
+  "Actors": [
+    "Tom Hanks",
+    "Michael Clarke Duncan",
+    "David Morse"
+  ]
+}`}
+                                </code>
+                            </pre>
+                        </div>
+                    </div>
+                </div>
+                <div className='case-study-section'>
+                    <LockIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+                        <p className='case-study-header-p'>
+                            Authentication
+                        </p>
+                        <div className="auth-section">
+                            <div className="label">AUTHENTICATION</div>
+                            <div className="desc">
+                                Basic HTTP authentication securely sends login details in the header, and the API verifies them with the database to log the user in.
+                            </div>
+
+                            <div className="label">AUTHORIZATION</div>
+                            <div className="desc">
+                                If the login is successful, a token is generated and sent with every request. Only users with a valid token can access the app, and the token’s complexity makes it very hard to crack.
+                            </div>
+
+                            <div className="label">SECURITY</div>
+                            <div className="desc">
+                                The movie API uses password hashing, converting passwords into secret codes before storing them. Even if the database is hacked, the real passwords stay hidden and only the user knows them.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='case-study-section'>
+                    <BuildIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+                        <p className='case-study-header-p'>
+                            Debugging
+                        </p>
+                        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                            <p>
+                                Postman is a tool that helps developers test how their web servers handle requests and responses.
+                            </p>
+                            <p>It’s like a digital assistant that helps you make sure everything is communicating properly.
+                            </p>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>
+                                <p>A</p> <p>B</p> <p>C</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='case-study-section'>
+                    <FlagIcon className="case-study-svgs" />
+                    <div className='case-study-header'>
+
+                        <p className='case-study-header-p'>
+                            Final Conclusion
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <button onClick={goBack} className="details_link case-study-button">
                 Go Back
             </button>
-        </div>
+        </div >
     );
 };
 
